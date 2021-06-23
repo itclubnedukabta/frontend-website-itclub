4954f5e0ba465811f13097f9ded7f41aefdaa5d0
alfatardiansa <alfat.ardi3105@gmail.com>
2021-06-24 02:20:46 +0700

    Home Page non responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import "../Tailwind.css";
-import "./Navbar.css";
 import { ReactComponent as ClassSvg } from "../assets/svg/class.svg";
 import { ReactComponent as HomeSvg } from "../assets/svg/home.svg";
 import { ReactComponent as InfoSvg } from "../assets/svg/info.svg";
@@ -13,7 +12,12 @@ import ITLogo from "../assets/img/it.png";
 class Navbar extends React.Component {
   render() {
     return (
-      <nav className="nav-bg z-50 lg:h-screen md:h-16 lg:py-12 lg:fixed sm:flex sm:items-center lg:px-0 lg:w-16 sm:justify-between lg:flex-col sm:px-8 md:px-16">
+      <nav
+        className="z-50 lg:h-screen md:h-16 lg:py-12 lg:fixed sm:flex sm:items-center lg:px-0 lg:w-16 sm:justify-between lg:flex-col sm:px-8 md:px-16"
+        style={{
+          backgroundImage: "linear-gradient(to right, #2F98C4, #1666A4)",
+        }}
+      >
         <div className="flex justify-center">
           <img
             src={ITLogo}
@@ -26,7 +30,7 @@ class Navbar extends React.Component {
           <NavBtn className="lg:my-4 lg:mx-0 sm:mx-4">
             <ClassSvg />
           </NavBtn>
-          <NavBtn className="lg:my-4 lg:mx-0 sm:mx-4" location="/home">
+          <NavBtn className="lg:my-4 lg:mx-0 sm:mx-4" location="/">
             <HomeSvg />
           </NavBtn>
           <NavBtn className="lg:my-4 lg:mx-0 sm:mx-4" location="/info">
